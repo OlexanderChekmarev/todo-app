d9dc168448bd2b88247f725fd48f1abc23b20c29
San4ez <alexanderchekmarev1987@gmail.com>
2024-01-12 19:59:13 +0200

    modified utils

---
+++ src/components/TodoDetails.tsx
@@ -0,0 +1,4 @@
+import { FunctionComponent } from "react";
+import { observer } from "mobx-react-lite";
+
+import styles from '../style/todos.modele.scss;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -22,3 +22,70 @@ type CompleteTogglerProps =(
 ) => TodoType[];
    
 
+export const subTaskAddingProps: subTaskAddingProps = (id, array, task) => {
+    return array.ruduce ((arr: TodoType[], item ) => {
+        if (item.id === id) {
+            item.subTasks.push(task);
+            arr.push(item);
+        } else {
+            arr.push({...item, subTasks: subTaskAddingProps(id,item.subTasks,task)});
+        }
+        return arr;
+        }, []);
+    };
+
+    export const recursionFilter:RecursionProps = (id,array) => {
+        return array.reduce((arr: TodoType[],item) => {
+            if (item.id !== id) {
+                arr.push({...item, subTasks: recursionFilter(id,item. subTasks)});
+            }
+            
+            return arr;
+        }, []);
+    };
+
+    export const recursionSearch:SearchProps = (id,array) => {
+        for (let item of array) {
+            if (item.id === id) {
+                return item;
+            }
+
+            const subItem = recursionSearch(id, item.subTasks);
+
+            if (subItem) {
+                return subItem;
+            }
+        }
+
+        return null;
+    };
+
+    export const recursionCompleteToggler:RecursionProps = (id, array) => {
+        return array.reduce((arr: TodoType[], item) => {
+            if (item.id !== id) {
+                arr.push({...item, subTasks: recursionCompleteToggler(id, item.subTasks)});
+            } else {
+                arr.push({
+                    ...item,
+                    isCompleted: !item.isCompleted,
+                    subTasks: subTasksCompleteToggler (item.subTasks, !item.isCompleted)
+                });
+            }
+
+            return arr;
+            },[]);
+        };
+
+        export const subTasksCompleteToggler:CompleteTogglerProps = (array, state) => {
+            return array.reduce((arr: TodoType[], item) => {
+              arr.push({
+                ...item, 
+                isCompleted: state, 
+                subTasks: subTasksCompleteToggler(item.subTasks, state)
+              });
+          
+              return arr;
+            }, []);
+          };
+    }
+    }
